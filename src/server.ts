c8ea12139a61ca4af0558ff264e8252ac9621f6c
Ericson Scodeler <ericson.br@hotmail.com>
2021-06-22 20:32:46 -0300

    configuração do typeorm

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,7 @@
 import express from 'express';
+import 'reflect-metadata';
+import './database';
 
 const app = express();
 
-app.get("/test", (req,resp) => {
-    return resp.send('Hello World Get')
-})
-
-app.post("/test-post",(req,resp) => {
-    return resp.send("Hello World Post")
-})
 app.listen(3000, () => console.log('Server is running NLW'))
