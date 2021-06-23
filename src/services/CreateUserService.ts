50c8424502267fe06ef1110e80cac52342b49671
Ericson Scodeler <ericson.br@hotmail.com>
2021-06-22 21:37:49 -0300

    Repository , Controller e Entity User

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,39 @@
+import {UserRepositories} from '../repositories/UserRepositories';
+import {getCustomRepository} from 'typeorm';
+
+
+interface IUserRequest {
+  name: string;
+  email: string;
+  admin?: boolean;
+}
+
+class CreateUserService {
+  async execute({ name, email, admin } : IUserRequest) {
+    const userRepository = getCustomRepository(UserRepositories)
+
+    if (!email){
+      throw new Error("Email incorrect")
+    }
+
+    const userAlreadyExists = await userRepository.findOne({
+      email
+    });
+
+    if(userAlreadyExists){
+      throw new Error("User already existes")
+    }
+
+    const user = userRepository.create({
+      name,
+      email,
+      admin
+    })
+
+    await userRepository.save(user);
+
+    return user;
+  }
+}
+
+export { CreateUserService }
